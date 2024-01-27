12197d9696794b652e6c274ec60d33b98e770f9b
benja <benjamin.heikkala@abo.fi>
2024-01-27 16:58:26 +0200

    added some files to gitignore

--- phonebookDocker/backend/index.js
+++ phonebookDocker/backend/index.js
@@ -5,8 +5,8 @@ var morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-morgan.token('object', function getObject (req,res) {
-  req.object=JSON.stringify(req.body)
+morgan.token('object', function getObject(req, res) {
+  req.object = JSON.stringify(req.body)
   return req.object
 })
 
@@ -18,35 +18,35 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :o
 const date = new Date()
 
 
-app.post('/api/persons', (request, response,next) => {
-  const {name, number} = request.body
+app.post('/api/persons', (request, response, next) => {
+  const { name, number } = request.body
 
   if (!name) {
     return response.status(400).json({
       error: 'fail: name missing'
     })
-  }else if(!number){
+  } else if (!number) {
     return response.status(400).json({
       error: 'fail: number missing'
     })
   }
 
-  const person = new Person( {
+  const person = new Person({
     name: name,
     number: number
   })
 
 
   Person.find({}).then(persons => {
-    const nameList= persons.map(p => p.name)
-    if(nameList.includes(person.name)){
+    const nameList = persons.map(p => p.name)
+    if (nameList.includes(person.name)) {
       Person.findByIdAndUpdate(person.id, person, { new: true })
         .then(updatedNumber => {
           response.json(updatedNumber)
         })
         .catch(error => next(error))
     }
-    else{
+    else {
       person.save().then(savedPerson => {
         response.json(savedPerson)
       }).catch(error => next(error))
@@ -57,10 +57,10 @@ app.post('/api/persons', (request, response,next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const { name,number } =request.body
+  const { name, number } = request.body
 
-  Person.findByIdAndUpdate(request.params.id, { name,number },
-    { new: true, runValidators:true, context:'query' })
+  Person.findByIdAndUpdate(request.params.id, { name, number },
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedNumber => {
       response.json(updatedNumber)
     })
@@ -78,9 +78,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
-      if (person){
+      if (person) {
         response.json(person)
-      }else{
+      } else {
         response.status(404).end
       }
 
@@ -110,7 +110,7 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError'){
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
 
